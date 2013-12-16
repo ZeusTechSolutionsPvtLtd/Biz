67e9d5a1e048354a85c3e3d2ccfd368c0ad100c9
Zeus <anandg@zeustechsolutions.com>
2013-12-16 09:22:50 -0800

    zeus

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Biz::Application.config.secret_key_base = 'cccf911fc15ef4044e6f0573a43e6fc576ffd38b4c65158444a3f6040f508cca6d0f2b359f08287dc49121f35d94cfc48a03eb935f84c0f772ff1e7d4d50d630'
