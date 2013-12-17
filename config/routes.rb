c802564c032f429f5ed9f90ee7e2da62fa213a9d
Zeus <anandg@zeustechsolutions.com>
2013-12-16 18:42:47 -0800

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Biz::Application.routes.draw do
+   root :to => 'layout#application'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
